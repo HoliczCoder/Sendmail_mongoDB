095580352f9615f9e2f298c37a80d6317798ec45
MinhNoob <minhtranconglis@gmail.com>
2023-01-30 18:03:52 +0700

    dev

--- src/middleware/basicAuth.ts
+++ src/middleware/basicAuth.ts
@@ -1,13 +1,31 @@
 import { Request, Response, NextFunction } from "express";
 
+// example users
+const users = [
+  { id: 1, name: "CR7", courses: [1, 2, 3], role: "ADM" },
+  { id: 2, name: "M10", courses: [1, 2, 8], role: "MOD" },
+  { id: 3, name: "RN9", courses: [1, 7, 4], role: "STU" },
+];
+
+function findUser(userId: number) {
+  return users.find((user) => user.id === userId);
+}
+
 export const authPage = (permisson: Array<String>) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    const { role } = req.body;
-    if (!role) {
+    const { userId } = req.body;
+    if (!userId) {
       return res.status(403).json({
         res: "you need to sign in",
       });
     }
+    const user = findUser(userId);
+    if (!user) {
+      return res.status(403).json({
+        res: "user not found",
+      });
+    }
+    const { role } = user;
     if (!permisson.includes(role)) {
       return res.status(401).json({
         res: "you dont have permisson",
@@ -17,3 +35,25 @@ export const authPage = (permisson: Array<String>) => {
   };
 };
 
+export const authCourse = (req: Request, res: Response, next: NextFunction) => {
+  const { userId } = req.body;
+  const { number } = req.params;
+  if (!userId) {
+    return res.status(403).json({
+      res: "you need to sign in",
+    });
+  }
+  const user = findUser(userId);
+  if (!user) {
+    return res.status(403).json({
+      res: "user not found",
+    });
+  }
+  const { courses } = user;
+  if (!courses.includes(+number)) {
+    return res.status(401).json({
+      res: "course not found",
+    });
+  }
+  next();
+};
--- src/routes/course.routes.ts
+++ src/routes/course.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import { Request, Response } from "express";
-import { authPage } from "../middleware/basicAuth";
+import { authPage, authCourse } from "../middleware/basicAuth";
 
 const router = Router();
-router.get("/get_list", authPage(['ADM', 'MOD']) ,(req, res, next) => {
+router.get("/get_list", authPage(["ADM", "MOD"]), (req, res, next) => {
   res.json({
     res: "get list student",
+  });
+});
+
+router.get("/:number", authCourse, (req, res, next) => {
+  res.json({
+    res: "you have accessed to this course",
   });
 });
 
